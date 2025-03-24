566a5c6237a8139365a11d5489d9ca51b7ed3fcc
kpfil <kpfil@HP-KLENE>
2025-03-23 21:14:03 -0400

    Add project files.

---
+++ Source.cpp
@@ -0,0 +1,14 @@
+// 3/23/25
+// 
+// This program displays the text "Hello World" to the console.
+
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello World!" << endl;
+	return 0;
+}
